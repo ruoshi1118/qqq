82484cb36db4aa311d6d076df5c151e20d9a0a31
Henry Tseng <henrybear327@gmail.com>
2017-05-16 13:52:19 +0800

    Fix link

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 $randomNumber = rand(0, 100);
 
+var_dump($randomNumber);
+
 if($randomNumber <= 50) {
     echo "五秒後前往 google.com<br>";
-    header("refresh:5;url=google.com");
+    header("refresh:5;url=https://google.com");
     die();
 } else {
     echo "五秒後前往 yahoo.com<br>";
-    header("refresh:5;url=yahoo.com");
+    header("refresh:5;url=https://yahoo.com");
     die();
 }
 ?>
