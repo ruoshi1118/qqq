2df38f10b0fa2bb1baa93cdcf4398ba97b98c4ad
Henry Tseng <henrybear327@gmail.com>
2017-05-16 13:51:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+$randomNumber = rand(0, 100);
 
-
-
-echo "Hello world!<br>";
+if($randomNumber <= 50) {
+    echo "五秒後前往 google.com<br>";
+    header("refresh:5;url=google.com");
+    die();
+} else {
+    echo "五秒後前往 yahoo.com<br>";
+    header("refresh:5;url=yahoo.com");
+    die();
+}
 ?>
